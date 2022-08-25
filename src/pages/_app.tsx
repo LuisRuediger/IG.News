00c7cf4c84589473a981d732d7868adadc97abbb
Luis Ruediger <luisruediger27@gmail.com>
2022-08-25 14:15:44 -0300

    global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from 'next/app'
 import React from 'react'
 
+import '../styles/global.scss'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styles from '../styles/home.module.scss'
 import Head from 'next/head'
 
 export default function Home() {
@@ -8,7 +7,7 @@ export default function Home() {
       <Head>
         <title>ig.news</title>
       </Head>
-      <h1 className={styles.title}>Hello World</h1>
+      <h1 >Hello World</h1>
     </>
   )
 }
