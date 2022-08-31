04c0b039cd8079992225e54712661a2a7430c2c3
Luis Ruediger <luisruediger27@gmail.com>
2022-08-31 14:25:05 -0300

    salvando user no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,10 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { signIn } from "next-auth/react"
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -12,7 +17,25 @@ export default NextAuth({
         },
       },
     }),
-  ]
+  ],
+  callbacks: {
+    async signIn({ user, account, profile}) {
+      const { email } = user;
+
+     try {
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          { data: { email } }
+        )
+      )
+      
+      return true
+     } catch {
+      return false
+     }
+    }
+  }
 })
 
 
